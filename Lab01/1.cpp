2830bc0a2cc6f0ae0db9ce2e6360d7c808314301
Muhammad Ibrahim <mibrahimfiftysix@gmail.com>
2025-08-25 19:00:16 +0500

    Update 1.cpp

--- Lab01/1.cpp
+++ Lab01/1.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 using namespace std;
 
-class BankAccount{
-    protected:
-    float *balance;//i used pointer here only to demonstrate the deep copy
+class BankAccount
+{
+protected:
+    float *balance; // i used pointer here only to demonstrate the deep copy
     string name;
-    public:
-    //default 
-    BankAccount() : name("") {
+
+public:
+    // default
+    BankAccount() : name("")
+    {
         balance = new float;
         *balance = 0.0;
     }
 
-    //parameterized
-    BankAccount(string name, float bal) : name(name){
+    // parameterized
+    BankAccount(string name, float bal) : name(name)
+    {
         balance = new float;
         *balance = bal;
     }
 
-    //copy const
-    BankAccount (const BankAccount& other) {
+    // copy const
+    BankAccount(const BankAccount &other)
+    {
         // this->name = other.name;
         cout << "Starting to copy\nEnter new name for account: ";
         cin >> this->name;
@@ -27,26 +32,32 @@ class BankAccount{
         *balance = *other.balance;
     }
 
-    ~BankAccount() {
+    ~BankAccount()
+    {
         delete balance;
     }
 
-    void displayDetails() {
+    void displayDetails()
+    {
         cout << "Name: " << name << "\nBalance: " << *balance << endl;
     }
 
-    void deduct(float money) {
-        if(*balance-money >= 0) {
+    void deduct(float money)
+    {
+        if (*balance - money >= 0)
+        {
             *balance -= money;
             cout << "Deducted " << money << endl;
         }
-        else {
+        else
+        {
             cout << "Insufficient Balance" << endl;
         }
     }
 };
 
-int main() {
+int main()
+{
     BankAccount acc1;
     acc1.displayDetails();
     cout << endl;
@@ -55,7 +66,6 @@ int main() {
     acc2.displayDetails();
     cout << endl;
 
-
     BankAccount acc3(acc2);
     cout << endl;
     acc3.deduct(200);
@@ -63,5 +73,4 @@ int main() {
     cout << endl;
     acc2.displayDetails();
     cout << endl;
-
 }
