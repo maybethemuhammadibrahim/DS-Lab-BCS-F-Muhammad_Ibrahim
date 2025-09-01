70ed3bddd32cd3d1d76342eea00c5ff938822df2
maybethemuhammadibrahim <mibrahimfiftysix@gmail.com>
2025-09-01 17:57:20 +0500

    code formatting

--- Lab02/4.cpp
+++ Lab02/4.cpp
@@ -1,50 +1,66 @@
+/*
+Create a dynamic program that manages monthly expenses for a family.
+=> Use a 1D dynamic array to store expenses for n months.
+=> Add a feature to resize the array if the user wants to track more months later.
+=> Compute and display the total and average expenses.
+*/
+/*Used Prettier(Vs Code extention for formatting code)*/
 #include <iostream>
 using namespace std;
 
-void addNewMonths(float *&arr, int &elements) {
+void addNewMonths(float *&arr, int &elements)
+{
     int additional;
     cout << "Enter additional number of months: ";
     cin >> additional;
 
     float *temp = new float[elements];
-    for(int i = 0; i < elements; i++) {
+    for (int i = 0; i < elements; i++)
+    {
         temp[i] = arr[i];
     }
 
     delete[] arr;
 
     arr = new float[elements + additional];
-    for(int i = 0; i < elements; i++) {
+    for (int i = 0; i < elements; i++)
+    {
         arr[i] = temp[i];
     }
 
     delete[] temp;
 
-    for(int j = 0; j < additional; j++) {
+    for (int j = 0; j < additional; j++)
+    {
         cout << "Enter expenses for month " << elements + j + 1 << " : ";
-        cin >> arr[elements + j]; 
+        cin >> arr[elements + j];
     }
 
-    elements += additional; 
+    elements += additional;
 }
 
-void displayTotalExpenses(float *arr, int elements) {
+void displayTotalExpenses(float *arr, int elements)
+{
     float total = 0;
-    for(int i = 0; i < elements; i++) {
+    for (int i = 0; i < elements; i++)
+    {
         total += arr[i];
     }
     cout << "Total Expenses = " << total << endl;
 }
 
-void displayAverageExpenses(float *arr, int elements) {
+void displayAverageExpenses(float *arr, int elements)
+{
     float total = 0;
-    for(int i = 0; i < elements; i++) {
+    for (int i = 0; i < elements; i++)
+    {
         total += arr[i];
     }
-    cout << "Average Expenses = " << total/elements << endl;
+    cout << "Average Expenses = " << total / elements << endl;
 }
 
-int main() {
+int main()
+{
     float *expenses;
     int noOfMonths = 0;
 
@@ -52,13 +68,15 @@ int main() {
     cin >> noOfMonths;
 
     expenses = new float[noOfMonths];
-    for(int i = 0; i < noOfMonths; i++) {
-        cout << "Enter expenses for month " << i+1 << " : ";
+    for (int i = 0; i < noOfMonths; i++)
+    {
+        cout << "Enter expenses for month " << i + 1 << " : ";
         cin >> expenses[i];
     }
 
     int input = -1;
-    do {
+    do
+    {
         cout << "\nEnter operation to perform\n"
              << "(1 - Resize and add expenses for more months)\n"
              << "(2 - Compute total expenses)\n"
@@ -66,7 +84,8 @@ int main() {
              << "(0 - Exit)" << endl;
         cin >> input;
 
-        switch (input) {
+        switch (input)
+        {
         case 1:
             addNewMonths(expenses, noOfMonths);
             break;
@@ -81,8 +100,8 @@ int main() {
         default:
             cout << "Invalid choice!\n";
         }
-    } while(input != 0);
+    } while (input != 0);
 
-    delete[] expenses; 
+    delete[] expenses;
     return 0;
 }
