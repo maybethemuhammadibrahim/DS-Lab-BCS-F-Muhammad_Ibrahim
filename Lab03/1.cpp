1c3998815026346d361b6531f30b2119f9a9fd41
Muhammad Ibrahim <mibrahimfiftysix@gmail.com>
2025-09-12 14:47:35 +0500

    upload

--- Lab03/1.cpp
+++ Lab03/1.cpp
@@ -13,7 +13,7 @@ class Node {
     int data;
     Node* next;
     public:
-    Node(int data = 0, Node* next = nullptr) : data(data) , next(next) {}
+    Node(int data = 0, Node *next = nullptr) : data(data) , next(next) {}
 
     int getData() {
         return data;
@@ -25,6 +25,10 @@ class Node {
 
     Node* getNext() {
         return next;
+    }
+
+    ~Node() {
+        delete next;
     }
 };
 
@@ -41,7 +45,7 @@ class LinkedList {
         }
         Node *current = head;
         int i = 0;
-        while(current->getNext() != nullptr) {
+        while(current != nullptr) {
             cout << i+1 << " :" << current->getData() << endl;
             current = current->getNext();
             i++;
@@ -50,18 +54,49 @@ class LinkedList {
     }
 
     void insertAtEnd(int newData) {
-        
+        Node *newNode = new Node(newData);
         if(head == nullptr) {
-            head->setNext(new Node(newData));
+            head = newNode;
+            cout << "Created new node at head" << endl;
+            return;
         }
 
         Node *current  = head;
         while(current->getNext() != nullptr) {
-            Node *newNode = new Node(newData);
-            newNode->setNext(current->getNext());
-            current->setNext(newNode);
             current = current->getNext();
         }
+        if(current->getNext() == nullptr) {
+            current->setNext(newNode);
+        }
+        
+    }
+
+    bool isPalindrome() {
+        Node *currentHead, *tempCurrent, *tempTail;
+        currentHead = head;
+        Node *currentTail = nullptr;
+        while (currentHead->getNext() != nullptr) {
+
+            //in each iteration of outer loop this inner loop will
+            //make currentTail point to (size - i) node
+            
+            //while currentHead is a pointer that increments 
+            //by one node in each iteration
+            tempCurrent = head;
+            while(tempCurrent->getNext() != currentTail) {
+                tempCurrent = tempCurrent->getNext();
+            }
+
+            currentTail = tempCurrent;
+
+            if(currentHead->getData() != currentTail->getData()) {
+                return false;
+            }
+
+            currentHead = currentHead->getNext();
+
+        }
+        return true;
     }
     
     
@@ -71,12 +106,14 @@ class LinkedList {
 int main()
 {
     LinkedList l1;
+    // cout << "Created ll" << endl;
     l1.insertAtEnd(10);
     l1.insertAtEnd(20);
     l1.insertAtEnd(30);
-    l1.insertAtEnd(40);
-    l1.insertAtEnd(50);
-
-    l1.display();
+    l1.insertAtEnd(30);
+    l1.insertAtEnd(20);
+    l1.insertAtEnd(10);
+    // l1.display();
+    cout << l1.isPalindrome() << endl;
     return 0;
 }
