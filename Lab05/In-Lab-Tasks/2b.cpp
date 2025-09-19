57d684d7dcdbaeccc87ea654504fd386fb1857e6
Muhammad Ibrahim <mibrahimfiftysix@gmail.com>
2025-09-19 12:11:27 +0500

    2b

---
+++ Lab05/In-Lab-Tasks/2b.cpp
@@ -0,0 +1,42 @@
+/*
+Write a simple C++ program that demonstrates direct recursion. Create a function
+printNumbers(int n) that prints numbers from n down to 1 by calling itself
+directly.
+*/
+
+#include <iostream>
+using namespace std;
+
+void printFunctionB(int n);
+
+void printFunctionA(int n) {
+    if (n == 0) {
+        cout << "0" << endl;
+        return;
+    }
+    else {
+        cout << n << endl;
+        printFunctionB(n-1);
+    }
+}
+
+void printFunctionB(int n) {
+    if (n == 0) {
+        cout << "0" << endl;
+        return;
+    }
+    else {
+        cout << n << endl;
+        printFunctionA(n-1); //calling A
+    }
+}
+
+int main() {
+    cout << "Enter a number:" ;
+    int n;
+    cin >> n;
+    cout << "Counting till 0: " << endl;
+    printFunctionA(n);
+    return 0;
+    
+}
